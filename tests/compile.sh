9ba17a1f7cffefe945209679078db900139bc9ae
HPDell <huyg0180110559@outlook.com>
2025-07-18 12:18:32 +0100

    add: skip files

--- tests/compile.sh
+++ tests/compile.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 TEST_DIR=$(pwd)
-for file in `ls src/*.typ`; do
+SKIPS_FILE="$TEST_DIR/skips.txt"
+find "$TEST_DIR/src" -name "*.typ" -type f | while read -r file; do
+  FILE_NAME=$(basename "$file")
+  if grep -Fxq "$FILE_NAME" "$SKIPS_FILE"; then
+    echo "Skipping $file"
+    continue
+  fi
   echo "Compiling $file"
-  typst compile --root `dirname $TEST_DIR` --format png "$TEST_DIR/$file" "$TEST_DIR/ref/$(basename "$file" .typ).png"
+  typst compile --root `dirname $TEST_DIR` --format png "$file" "$TEST_DIR/ref/$(basename "$FILE_NAME" .typ).png"
 done
